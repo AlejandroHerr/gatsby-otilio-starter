db329c32a64aeb95450938879fb6808bfa88384e
Alejandro Hernandez <alejandro@brainhub.pl>
2018-03-30 17:16:15 +0200

    Fix: Fixed functional component

--- src/html.js
+++ src/html.js
@@ -10,41 +10,40 @@ if (process.env.NODE_ENV === 'production') {
   }
 }
 
-module.exports = {
-  render() {
-    let css;
-    if (process.env.NODE_ENV === 'production') {
-      css = (
-        <style
-          id="gatsby-inlined-css"
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
-        />
-      );
-    }
-
-    return (
-      <html {...this.props.htmlAttributes}>
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <meta name="google-site-verification" content="KDJIl1u1p0fYHcU-7bRK_yd6TbErCKzLlEbxjPqE0EU" />
-          {css}
-          {this.props.headComponents}
-        </head>
-        <body {...this.props.bodyAttributes}>
-          {this.props.preBodyComponents}
-          <div
-            key="body"
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-        </body>
-      </html>
+module.exports = (props) => {
+  let css;
+  if (process.env.NODE_ENV === 'production') {
+    css = (
+      <style
+        id="gatsby-inlined-css"
+        dangerouslySetInnerHTML={{ __html: stylesStr }}
+      />
     );
-  },
+  }
+
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="google-site-verification" content="KDJIl1u1p0fYHcU-7bRK_yd6TbErCKzLlEbxjPqE0EU" />
+        {css}
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+
+      </body>
+    </html>
+  );
 };
