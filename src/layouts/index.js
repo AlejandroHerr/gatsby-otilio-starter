e21a0e7e6d0722b275d0d37562bc9ef6db5550cb
Alejandro Hernandez <alejandro@brainhub.pl>
2018-03-31 22:04:43 +0200

    Feat: Added cannonical url

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
 import '../global.scss';
 
-const Template = ({ children }) => (
-  <div>{children()}</div>
+const Template = ({
+  children, data, location,
+}) => (
+  <div>
+    <Helmet>
+      <link rel="canonical" href={`${data.siteInfo.childDataJson.url}${location.pathname}`} />
+    </Helmet>
+    {children()}
+  </div>
 );
 
 Template.propTypes = {
   children: PropTypes.func.isRequired,
+  data: PropTypes.shape({
+    siteInfo: PropTypes.shape({
+      childDataJson: PropTypes.shape({
+        url: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Template;
+
+/* eslint-disable */
+export const pageQuery = graphql`
+  query LayourQUery {
+    siteInfo: file(relativePath: { eq: "siteInfo.json" }) {
+      childDataJson {
+        url,
+      }
+    }
+  }
+`;
