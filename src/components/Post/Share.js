7bcaaa1840530ec76a329c611d9e765df4a5060b
Alejandro Hernandez <alejandro@brainhub.pl>
2018-03-31 17:19:55 +0200

    Feat: Added Share section in posts

--- src/components/Post/Share.js
+++ src/components/Post/Share.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FacebookShareButton } from 'react-share';
-import SimpleIcon from '../icons/SimpleIcon';
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+} from 'react-share';
 
 import styles from './Post.module.scss';
 
 const Share = ({ path, url }) => (
   <div className={styles.post_share}>
     <FacebookShareButton url={`${url}${path}`} className={styles.post_share__button}>
-      <SimpleIcon brand="facebook" className={styles.post_share__facebook} />
+      <FacebookIcon size="100%" />
     </FacebookShareButton>
+    <LinkedinShareButton url={`${url}${path}`} className={styles.post_share__button}>
+      <LinkedinIcon size="100%" />
+    </LinkedinShareButton>
+    <TwitterShareButton url={`${url}${path}`} className={styles.post_share__button}>
+      <TwitterIcon size="100%" />
+    </TwitterShareButton>
+    <WhatsappShareButton url={`${url}${path}`} className={styles.post_share__button}>
+      <WhatsappIcon size="100%" />
+    </WhatsappShareButton>
   </div>
 );
 
