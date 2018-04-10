d9d2eda24d0244927c107806ea244c6c45a35353
Alejandro Hernandez <alejandro@brainhub.pl>
2018-04-10 23:06:47 +0200

    Refactor: Changes proptypes on Share component

--- src/components/Post/Share.js
+++ src/components/Post/Share.js
@@ -14,25 +14,21 @@ import WhatsappIcon from './shareIcons/WhatsappIcon';
 
 import styles from './Post.module.scss';
 
-const Share = ({ path, url }: {path?: string, url: string}) => (
+const Share = ({ path, url }: {path: string, url: string}) => (
   <div className={styles.post_share}>
-    <FacebookShareButton url={`${url}${path || ''}`} className={styles.post_share__button}>
+    <FacebookShareButton url={`${url}${path}`} className={styles.post_share__button}>
       <FacebookIcon size="100%" />
     </FacebookShareButton>
-    <LinkedinShareButton url={`${url}${path || ''}`} className={styles.post_share__button}>
+    <LinkedinShareButton url={`${url}${path}`} className={styles.post_share__button}>
       <LinkedinIcon size="100%" />
     </LinkedinShareButton>
-    <TwitterShareButton url={`${url}${path || ''}`} className={styles.post_share__button}>
+    <TwitterShareButton url={`${url}${path}`} className={styles.post_share__button}>
       <TwitterIcon size="100%" />
     </TwitterShareButton>
-    <WhatsappShareButton url={`${url}${path || ''}`} className={styles.post_share__button}>
+    <WhatsappShareButton url={`${url}${path}`} className={styles.post_share__button}>
       <WhatsappIcon size="100%" />
     </WhatsappShareButton>
   </div>
 );
-
-Share.defaultProps = {
-  path: '',
-};
 
 export default Share;
